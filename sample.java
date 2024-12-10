57936b078f064e5edc1e811cbf36f4e2c5f70cea
simituli <anupama.tuli@gmail.com>
2024-12-10 13:51:34 -0800

    Update sample.java - Longest increasing subsequence

--- sample.java
+++ sample.java
@@ -1,7 +1,26 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n*2)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : yes, compilation error plus logical error
 
-
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int result = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    result = Math.max(result, dp[i]);
+                }
+            }
+        }
+        return result;
+    }
+}
 // Your code here along with comments explaining your approach
